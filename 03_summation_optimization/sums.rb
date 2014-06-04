9123416000ce8493ac0368871cd5d14ae981a1bf
Blake Thomas <bwthomas@gmail.com>
2014-06-04 09:00:11 -0500

    You have 1.5 hours to make it go faster.

--- 03_summation_optimization/sums.rb
+++ 03_summation_optimization/sums.rb
@@ -1,27 +1,60 @@
 require 'benchmark'
 
-## > which ruby
-## /opt/rubies/ruby-1.9.3-p484/bin/ruby
+## /opt/rubies/rbx-2.2.3/bin/ruby
 ## > ruby 03_summation_optimization/sums.rb
 ##        user     system      total        real
 ##  last six numbers: 940314
-##   5.030000   0.040000   5.070000 (  5.065199)
+##   3.682826   0.088958   3.771784 (  3.592792)
+##  last six numbers: 940314
+##   7.010272   0.396651   7.406923 (  1.503909)
 ##
 ## (20140604, BWT)
 
-Benchmark.bm do |x|
-  x.report do
-    n = 3500
-    total = 0
+class Summit
+  attr_accessor :n, :total
+
+  def initialize(n)
+    @n = n
+    @total = 0
+  end
+
+  def threadless
+    @total = 0
+    outer = []
 
     n.times do |i|
-      sum = 1
-      i.times do |j|
-        sum *= j+1
-      end
-      total += sum
+      a = [1] + (1..i).to_a
+      outer << a.inject(:*)
     end
 
-    puts "last six numbers: #{total % 1000000}"
+    @total = outer.inject(:+)
+    puts "last six numbers: #{@total % 1000000}"
+    @total
   end
+
+  def threaded(concurrency = 32)
+    @total = 0
+    outer       = []
+    bundle_size = n / concurrency + 1
+
+    (0..n-1).each_slice(bundle_size) do |m|
+      @th = Thread.new do
+        m.each do |i|
+          a = [1] + (1..i).to_a
+          outer << a.inject(:*)
+        end
+      end
+    end
+
+    @th.join
+    @total = outer.inject(:+)
+    puts "last six numbers: #{@total % 1000000}"
+    @total
+  end
+end
+
+Benchmark.bm do |x|
+  s = Summit.new(3500)
+  x.report { s.threadless }
+  x.report { s.threaded }
 end
