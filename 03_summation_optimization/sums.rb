976c3be1f603e7088d79c50eb83df242eabea580
Blake Thomas <bwthomas@gmail.com>
2014-06-04 08:58:59 -0500

    Change n in sums.rb so that when you run time ruby sums.rb 3 times in a row it gives you roughly 5 seconds.

--- 03_summation_optimization/sums.rb
+++ 03_summation_optimization/sums.rb
@@ -1,12 +1,27 @@
-n = 2680
-total = 0
+require 'benchmark'
 
-n.times do |i|
-  sum = 1
-  i.times do |j|
-    sum *= j+1
+## > which ruby
+## /opt/rubies/ruby-1.9.3-p484/bin/ruby
+## > ruby 03_summation_optimization/sums.rb
+##        user     system      total        real
+##  last six numbers: 940314
+##   5.030000   0.040000   5.070000 (  5.065199)
+##
+## (20140604, BWT)
+
+Benchmark.bm do |x|
+  x.report do
+    n = 3500
+    total = 0
+
+    n.times do |i|
+      sum = 1
+      i.times do |j|
+        sum *= j+1
+      end
+      total += sum
+    end
+
+    puts "last six numbers: #{total % 1000000}"
   end
-  total += sum
 end
-
-puts "last six numbers: #{total % 1000000}"
